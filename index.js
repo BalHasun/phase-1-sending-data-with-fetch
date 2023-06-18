12e8b3fb569d433f1925f7fcfcae1d398416f42b
Bal Elsada Hasun <fouadhason@gmail.com>
2023-06-18 19:55:28 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Add your code here
+const rando = Math.ceil(Math.random() * 1000);
+
+function submitData(name, email) {
+  const formData = {
+    id: rando,
+    name: name.value,
+    email: email.value,
+  };
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  fetch("http://localhost:3000/users ", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+    .catch(function (error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });
+}
+function handleSubimt(event) {
+  event.preventDefault();
+  const name = document.querySelector("input#userName");
+  const email = document.querySelector("input#userEmail");
+  submitData(name, email);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", handleSubimt);
+});
